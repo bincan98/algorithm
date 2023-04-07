612ab6dabcecdda275967608f308051d2b7eed13
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-04-07 10:59:10 +0900

    [Gold III] Title: 연구소 3, Time: 232 ms, Memory: 18592 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -9,31 +9,31 @@ import java.util.StringTokenizer;
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	private static int N, M;
-	private static int[][] arr;
-	private static int[][] virusLocation;
-	private static int cnt;
+	private static int N, M, cnt, answer;
+	private static int[][] arr, virusLocation, distance;
 	private static int[] numbers;
 	private static int dy[] = {0,0,-1,1};
 	private static int dx[] = {-1,1,0,0};
-	private static int[][] distance;
-	private static int answer;
 
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
+		// 연구소 저장할 행렬
 		arr = new int[N][N];
+		// 모든 바이러스 위치 저장할 배열 [0]:y좌표, [1]:x좌표
 		virusLocation = new int[10][2];
+		// 조합으로 고른 바이러스의 인덱스 저장할 배열
 		numbers = new int[M];
 
+		// 바이러스 개수
 		cnt = 0;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				int ele = Integer.parseInt(st.nextToken());
-				if(ele == 2) {
+				if(ele == 2) { // 바이러스 라면 배열에 저장하고 cnt 올리기
 					virusLocation[cnt][0] = i;
 					virusLocation[cnt][1] = j;
 					cnt++;
@@ -44,16 +44,19 @@ public class Main {
 		} // 입력 완료
 
 		answer = Integer.MAX_VALUE;
+		// 조합 뽑기
 		combi(0, 0);
 		
 		System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
 	} // end of main
 
 	private static void combi(int index, int k) {
-		if(index == M) {
+		if(index == M) { // 조합 완성되었다면
 
+			// 바이러스 활성화 시켜서 퍼뜨리기
 			activate();
 			
+			// 최소시간 계산하기
 			calculate();
 			return;
 		}
@@ -67,7 +70,7 @@ public class Main {
 		int max = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if(distance[i][j] == -1 && arr[i][j] != 1) return;
+				if(distance[i][j] == -1 && arr[i][j] != 1) return; // 만약 벽이 아닌데 탐색되지 못한곳이 있다면 계산 종료
 				if(arr[i][j] != 2) max = Math.max(max, distance[i][j]);
 			}
 		}
@@ -78,8 +81,10 @@ public class Main {
 	private static void activate() {
 
 		Queue<Integer> queue = new ArrayDeque<>();
+		// 최소시간 저장할 행렬
 		distance = new int[N][N];
 		
+		// 행렬의 초기값은 -1로
 		for (int i = 0; i < N; i++) {
 			Arrays.fill(distance[i], -1);			
 		}
@@ -98,10 +103,11 @@ public class Main {
 			for (int i = 0; i < 4; i++) {
 				int ny = currentY + dy[i];
 				int nx = currentX + dx[i];
-				if(isVaild(ny,nx) && arr[ny][nx] != 1) {
-					if (distance[ny][nx] == -1) {
+				if(isVaild(ny,nx) && arr[ny][nx] != 1) { // 행렬의 범위안에 포함되고 벽이 아니라면
+					if (distance[ny][nx] == -1) { // distance로 visited 체크하고 
 						queue.offer(ny);
 						queue.offer(nx);
+						// 최소시간 추가하기
 						distance[ny][nx] = distance[currentY][currentX] + 1;
 					}
 				}
