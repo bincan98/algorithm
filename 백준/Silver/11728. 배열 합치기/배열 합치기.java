7a4cd8e69f6a44ee39ca2aa7b0df908221588b5b
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-04-16 03:12:45 +0900

    [Silver V] Title: 배열 합치기, Time: 1156 ms, Memory: 344016 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -9,29 +9,54 @@ public class Main {
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 	private static int N, M;
-	private static int[] arr;
+	private static int[] arr, arrA, arrB;
 	
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
+		arrA = new int[N];
+		arrB = new int[M];
 		arr = new int[N+M];
 		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken()); 
+			arrA[i] = Integer.parseInt(st.nextToken()); 
 		}
 		
 		st = new StringTokenizer(br.readLine());
-		for (int i = N; i < N+M; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < M; i++) {
+			arrB[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(arr);
+//		Arrays.sort(arr);
+		int currentA = 0;
+		int currentB = 0;
+		int i = 0;
+		while(true) {
+			if(arrA[currentA] >= arrB[currentB]) {
+				arr[i++] = arrB[currentB++];
+			} else {
+				arr[i++] = arrA[currentA++];
+			}
+			
+			if(currentA == N) {
+				for (int j = currentB; j < M; j++) {
+					arr[i++] = arrB[j];
+				}
+				break;
+			}
+			if(currentB == M) {
+				for (int j = currentA; j < N; j++) {
+					arr[i++] = arrA[j];
+				}
+				break;
+			}
+		}
 		
-		for (int i = 0; i < N+M; i++) {
-			sb.append(arr[i]).append(" ");
+		for (int k = 0; k < N+M; k++) {
+			sb.append(arr[k]).append(" ");
 		}
 		
 		System.out.println(sb.toString());
