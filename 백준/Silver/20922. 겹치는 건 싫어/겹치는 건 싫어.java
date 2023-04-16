49502d6d6ddde6e8b23d0b98bc7a52f065e34337
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-04-16 15:44:57 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 292 ms, Memory: 37320 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -15,30 +15,30 @@ public class Main {
     K = Integer.parseInt(st.nextToken());
 
     arr = new int[N + 1];
-    check = new int[100_001];
+    check = new int[100_001]; // 수를 넣어가면서 K가 넘었는지 체크할 배열
 
     st = new StringTokenizer(br.readLine());
     for (int i = 1; i < N+1; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     } // 입력완료
 
-    int answer = 0;
-
+    int answer = 0; // 정답
+    // 초기값 넣기
     int start = 1;
     int end = 1;
     check[arr[end]]++;
-    
+
     while (true) {
-      if (check[arr[end]] <= K) {
-        answer = Math.max(answer, end - start + 1);
-        end++;
-        if (end > N)
+      if (check[arr[end]] <= K) { // 새로 넣은 수가 K개를 넘지 않았다면 
+        answer = Math.max(answer, end - start + 1); // 정답에 현재 길이 넣어주기
+        end++; // 엔드 포인터 증가시키기
+        if (end > N) // 배열을 벗어났다면 종료
           break;
-        else 
-          check[arr[end]]++;
-      } else {
-        check[arr[start]]--;
-        start++;
+        else // 배열을 벗어나지 않았다면
+          check[arr[end]]++; // 엔드 포인터의 개수 1 증가시키기
+      } else { // 새로넣은 수가 K개를 넘었다면
+        check[arr[start]]--; // 스타트 포인터의 개수 감소시키기
+        start++; // 스타트 포인터 증가시키기
       }
     }
 
