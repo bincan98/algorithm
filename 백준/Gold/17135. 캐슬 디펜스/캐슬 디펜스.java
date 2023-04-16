846c84a5091d600f5cb3363671affcba0b7d785d
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-04-17 00:31:46 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 196 ms, Memory: 29796 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -8,13 +8,11 @@ import java.util.StringTokenizer;
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	private static int N, M, D;
+	private static int N, M, D, result, answer;
 	private static int[] numbers;
 	private static int[][] arr, arrCopy;
 	static int[] dy = { 0, -1, 0 };
 	static int[] dx = { -1, 0, 1 };
-	private static int result;
-	private static int answer;
 
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
@@ -31,24 +29,28 @@ public class Main {
 			}
 		} // 입력완료
 
+		// 궁수 위치를 저장할 배열
 		numbers = new int[3];
+		// 궁수 위치를 조합으로 구해주기
 		combi(0, 0);
 		
 		System.out.println(answer);
 	} // end of main
 
 	private static void combi(int index, int start) {
-		if (index == 3) {
+		if (index == 3) { // 조합 완성
 			// 새로운 배열에 맵 복사하기
 			arrCopy = new int[N][M];
 			for (int i = 0; i < N; i++) {
 				System.arraycopy(arr[i], 0, arrCopy[i], 0, arr[i].length);
 			}
+			// 이번 조합에서 잡을 수 있는 적의 수 초기화
 			result = 0;
-			while (check()) {
-				attack();
-				down();
+			while (check()) { // 현재 맵에 적이 있는지 확인하기
+				attack(); // 공격하기
+				down(); // 적 내려오기
 			}
+			// answer에 이번 조합에서의 제거한 수 넣어보기
 			answer = Math.max(answer, result);
 			return;
 		}
@@ -59,21 +61,27 @@ public class Main {
 	} // end of combi
 
 	private static void attack() {
+		// 이번 공격에서 제거할 적의 좌표들을 저장할 배열
 		int[][] targets = new int[3][2];
 		for (int i = 0; i < 3; i++) {
+			// 적의 좌표 구해오기
 			targets[i] = bfs(N-1, numbers[i]);
 		}
+		// 구해온 좌표들 처리하기
 		for (int i = 0; i < 3; i++) {
+			// 적을 구해지 못했다면 다음 좌표 살피기
 			if(targets[i][0] == -1) continue;
 			else {
+				// result에 이번 좌표의값 넣기
 				result += arrCopy[targets[i][0]][targets[i][1]];
+				// 적을 제거했으므로 좌표 값 0으로 만들기
 				arrCopy[targets[i][0]][targets[i][1]] = 0;
 			}			
 		}
 	} // end of attack
 
 	private static int[] bfs(int startY, int startX) {
-
+		// 반환할 적의 좌표 배열 생성하고 초기값 넣기
 		int[] targetArr = new int[2];
 		targetArr[0] = -1;
 		targetArr[1] = -1;
@@ -85,30 +93,32 @@ public class Main {
 		queue.offer(startX);
 		visited[startY][startX] = true;
 
-		if (arrCopy[startY][startX] == 1) {
-			targetArr[0] = startY;
+		if (arrCopy[startY][startX] == 1) {	// 이미 적이 있다면 
+			targetArr[0] = startY; // 배열에 값 넣기
 			targetArr[1] = startX;
-			return targetArr;
+			return targetArr; // 바로 리턴하기
 		}
 
-		int count = 1;
+		int count = 1; // D와 비교할 궁수의 위치에서 좌표까지의 거리
 		while (true) {
 			count++;
-			if(count > D) break;
-			int s = queue.size()/2;
+			if(count > D) break; // 거리가 벗어났다면 나가기
+			int s = queue.size()/2; // count를 쓰기위한 큐의 사이즈(큐에 두개씩 넣고 빼므로 2로 나누기) 
 			for (int i = 0; i < s; i++) {
 				int currentY = queue.poll();
 				int currentX = queue.poll();
 				for (int j = 0; j < 3; j++) {
+					// 왼쪽부터 살피도록 dy, dx를 선언하기
 					int ny = currentY + dy[j];
 					int nx = currentX + dx[j];
 					if (isValid(ny, nx) && !visited[ny][nx]) {
-						if (arrCopy[ny][nx] == 1) {
-							targetArr[0] = ny;
+						if (arrCopy[ny][nx] == 1) { // 적을 발견했다면
+							targetArr[0] = ny; // 배열에 값 넣기
 							targetArr[1] = nx;
+							// 바로 리턴하기
 							return targetArr;
-						} else {
-							queue.offer(ny);
+						} else { // 적이 없다면
+							queue.offer(ny); // 큐에 넣어주고 방문체크하기
 							queue.offer(nx);
 							visited[ny][nx] = true;
 						}
@@ -116,31 +126,34 @@ public class Main {
 				}
 			}
 		} // end of while
+		
 		return targetArr;
 
 	} // end of bfs
 
-
+	// 범위 체크
 	private static boolean isValid(int y, int x) {
 		return 0<=y && y<N && 0<=x && x<M;
 	} // end of isValid
 
-	private static void down() {
-		for (int i = N - 2; i >= 0; i--) {
+	private static void down() { // 적 하나씩 내리기
+		for (int i = N - 2; i >= 0; i--) { // 아래부터 시작
 			for (int j = 0; j < M; j++) {
+				// 한칸 위에서 적들을 내려오게 하기 
 				arrCopy[i + 1][j] = arrCopy[i][j];
 			}
 		}
+		// 첫줄은 0으로 초기화
 		for (int i = 0; i < M; i++) {
 			arrCopy[0][i] = 0;
 		}
 	} // end of down
 
-	private static boolean check() {
+	private static boolean check() { // 적이 맵에 있는지 체크
 		boolean ch = false;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (arrCopy[i][j] == 1) {
+				if (arrCopy[i][j] == 1) { // 적이 있다면 true로 변경
 					ch = true;
 					break;
 				}
