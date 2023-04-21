9a19a35e5be75121c0488857e856cca8fedd2362
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-04-21 16:14:39 +0900

    [Silver III] Title: 수열, Time: 144 ms, Memory: 126012 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,19 +1,21 @@
+# 입력 최적화
 import sys
-
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 
+# 처음 수열 합 구하기
 sum = 0
 for i in range(K+1):
     sum += arr[i]
-
 answer = sum
 
+# 하나씩 밀면서 가기
 for i in range(1, N-K+1):
     sum -= arr[i]
     sum += arr[i+K]
+    # answer에 넣어보기
     answer = max(answer, sum)
 
 print(answer)
