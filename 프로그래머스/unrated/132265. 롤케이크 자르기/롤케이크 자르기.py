64a4dc4aebabb6fe949d1b2cfbd66522d3b361e8
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-04-27 13:22:31 +0900

    [unrated] Title: 롤케이크 자르기, Time: 352.22 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,30 +1,32 @@
 def solution(topping):
     
-    chulsoo = [0] * 10001
-    bro = [0] * 10001
+    chulsoo = [0] * 10001 # 철수 토핑 배열
+    bro = [0] * 10001 # 동생 토핑 배열
     
-    chulsooKind = 0
-    broKind = 0
+    chulsooKind = 0 # 철수 토핑 종류
+    broKind = 0 # 동생 토핑 종류
     
+    # 철수한테 다 주기
     for top in topping:
-        if chulsoo[top] == 0: 
-            chulsooKind += 1
-        chulsoo[top] += 1
-
-    #print(chulsoo)
-    #print(chulsooKind)
+        if chulsoo[top] == 0: # 새로 추가된 토핑이라면
+            chulsooKind += 1 # 종류 1 추가하기
+        chulsoo[top] += 1 # 토핑 배열에 넣기
     
-    answer = 0
+    answer = 0 # 공평하게 자르는 방법 수
+    # 하나씩 동생한테 주기
     for top in topping:
+        # 철수꺼 하나 뺏기
         chulsoo[top] -= 1
-        if chulsoo[top] == 0:
-            chulsooKind -= 1
+        if chulsoo[top] == 0: # 뺏어서 없어졌다면
+            chulsooKind -= 1 # 종류 줄이기
         
-        if bro[top] == 0:
-            broKind += 1
-        bro[top] +=1
+        # 동생에 추가해주기
+        if bro[top] == 0: # 동생 배열에 새로 추가되는 거라면
+            broKind += 1 # 종류 추가하기
+        bro[top] +=1 # 토핑 배열에 넣기
         
+        # 공평하게 나눴다면(둘의 토핑 종류가 같다면)
         if chulsooKind == broKind:
-            answer += 1
+            answer += 1 # answer 추가하기
     
     return answer
