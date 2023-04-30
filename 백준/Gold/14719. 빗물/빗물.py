241619b66461cb3d790f8e9612c04a919ce1e2c1
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-04-30 16:22:37 +0900

    [Gold V] Title: 빗물, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -2,19 +2,25 @@ import sys
 input = sys.stdin.readline
 
 H, W = map(int, input().split())
-lst = [0] + list(map(int, input().split())) + [0]
+lst = [0] + list(map(int, input().split())) + [0] # 양 끝에 0더해주기
 
+# 왼쪽 최대값 저장할 배열
 prefix_max = [0] * (W+2)
+# 오른쪽 최대값 저장할 배열
 suffix_max = [0] * (W+2)
 
+# 왼쪽부터 돌면서 최대값을 저장하기
 for i in range(1, W+1):
     prefix_max[i] = max(prefix_max[i-1], lst[i])
 
+# 오른쪽부터 돌면서 최대값을 저장하기
 for i in range(W, 0, -1):
     suffix_max[i] = max(suffix_max[i+1], lst[i])
 
 answer = 0
+# 왼쪽부터 돌면서
 for i in range(W):
-    answer += min(prefix_max[i], suffix_max[i]) - lst[i]
+    # 물이고이는양 = (왼쪽, 오른쪽 최대값 중 최솟값 - 원래의 블록) 을 answer에 더해주기 
+   answer += min(prefix_max[i], suffix_max[i]) - lst[i]
 
 print(answer)
