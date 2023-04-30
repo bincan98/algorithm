c3031364636290195feb7cb42eb2e2bb38b9d8c7
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-04-30 15:59:24 +0900

    [level 3] Title: 등굣길, Time: 5.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -1,18 +1,22 @@
 def solution(m, n, puddles):
+    # 웅덩이 저장할 2차원 배열
     maps = [[False] * (m + 1) for _ in range(n + 1)]
 
+    # 웅덩이 저장하기
     for puddle in puddles:
         maps[puddle[1]][puddle[0]] = True
 
+    # 최소거리 저장할 2차원 배열
     disCnt = [[0] * (m + 1) for _ in range(n + 1)]
 
+    # 시작하기
     disCnt[1][1] = 1
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
-            if not maps[i - 1][j]:
-                disCnt[i][j] += disCnt[i - 1][j] % 1000000007
-            if not maps[i][j - 1]:
-                disCnt[i][j] += disCnt[i][j - 1] % 1000000007
+            if not maps[i - 1][j]: # 위쪽 좌표가 웅덩이가 아니라면
+                disCnt[i][j] += disCnt[i - 1][j] % 1000000007 # 위쪽 방법수 더해주기
+            if not maps[i][j - 1]: # 왼쪽 좌표가 웅덩이가 아니라면
+                disCnt[i][j] += disCnt[i][j - 1] % 1000000007 # 왼쪽 방법수 더해주기
 
     return disCnt[n][m] % 1000000007
