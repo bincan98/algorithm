9b91e68d8a43df6d16cdb4a3cdd98a71c60d60b6
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-06-11 01:21:01 +0900

    [Silver II] Title: 보이는 점의 개수, Time: 212 ms, Memory: 12476 KB -BaekjoonHub

--- 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.java
+++ 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.java
@@ -2,14 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+1 3
+2 3 + 2
+3 3 + 2 + 4
+4 ... + 4
 
+어떤 좌표가 0,0 에 연결되는지 알아보는 방법
+x, y가 같은 수로 나누어지면 보이지 않는다
+이게핵심) 즉 같은 수로 나누어지지 않으면 됨
+즉 x, y의 최대공약수가 1이면 보이는 수이다
+**/
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
+		// 개수 저장할 배열
 		int[] dp = new int[1001];
 		
+		// 모든 좌표 다 구하기
 		for (int i = 1; i < 1001; i++) {
 			for (int j = 1; j < 1001; j++) {
 				if (gcd(Math.max(i, j), Math.min(i, j)) == 1) {
@@ -18,8 +30,10 @@ public class Main {
 			}
 		}
 		
+		// (0,1), (1,0) 더하기
 		dp[1] += 2;
 		
+		// 모든 개수 더하기
 		for (int i = 1; i < 1001; i++) {
 			dp[i] += dp[i-1];
 		}
@@ -39,7 +53,6 @@ public class Main {
 			a = temp;
 		}
 		return b;
-	}
-	
+	} // end of gcd
 	
 } // end of class
