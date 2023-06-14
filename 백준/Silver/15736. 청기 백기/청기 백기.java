97f01de6aeb95e2adf4953cf175b4a62d5947d59
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-06-15 04:28:43 +0900

    [Silver IV] Title: 청기 백기, Time: 80 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ 왜인지는 모름
+
+그냥 다 써보니까 제곱수만 백색임
+ */
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int N = Integer.parseInt(br.readLine());
+		
+		int answer = 0;
+		for (int i = 1; i < (int)Math.pow(N, 0.5)+1; i++) { // (int) 씌워주지 않으면 한번 더하게 된다
+			answer++;
+		}
+		
+		System.out.println(answer);
+		
+	} // end of main
+} // end of class
