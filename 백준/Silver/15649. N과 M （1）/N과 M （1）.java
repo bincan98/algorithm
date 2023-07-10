a27d695768ad42252d948cb13982e41cac887711
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-07-11 00:10:18 +0900

    [Silver III] Title: N과 M (1), Time: 2164 ms, Memory: 58760 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 수열 기본 코드
+ */
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	private static int N;
+	private static int M;
+	private static int[] selected;
+	private static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		selected = new int[M];
+		visited = new boolean[N+1];
+		
+		recur(0);
+	} // end of main
+
+	private static void recur(int cur) {
+		if (cur == M) {
+			for (int i : selected) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = 1; i < N+1; i++) {
+			if (visited[i]) continue;
+			visited[i] = true;
+			selected[cur] = i;
+			recur(cur + 1);
+			visited[i] = false;
+		}
+	} // end of recur
+} // end of class
