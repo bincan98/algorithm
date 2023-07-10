86cc9ab1e156d65106ce03b8630e864865533082
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-07-11 01:32:49 +0900

    [Silver III] Title: N과 M (7), Time: 892 ms, Memory: 408396 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 중복순열 기본 코드
+ */
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	private static int N;
+	private static int M;
+	private static int[] selected;
+	private static int[] lst;
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		selected = new int[M];
+		lst = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			lst[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(lst);
+		
+		recur(0);
+		
+		System.out.println(sb.toString());
+	} // end of main
+
+	private static void recur(int cur) {
+		if (cur == M) {
+			for (int i : selected) {
+				sb.append(lst[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			selected[cur] = i;
+			recur(cur + 1);
+		}
+	} // end of recur
+} // end of class
