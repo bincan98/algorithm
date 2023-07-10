d2aea246818046807bfe1cb32bbb4762606976b0
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-07-11 00:03:12 +0900

    [Silver III] Title: N과 M (1), Time: 200 ms, Memory: 118764 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,28 @@
+"""
+수열 기본코드
+"""
+
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+N, M = map(int, input().split())
+
+selected = [0] * M
+visited = [False] * (N+1)
+
+
+def recur(cur):
+	if cur == M:
+		print(*selected)
+		return
+	for i in range(1, N+1):
+		if visited[i]:
+			continue
+		selected[cur] = i
+		visited[i] = True
+		recur(cur + 1)
+		visited[i] = False
+
+
+recur(0)
