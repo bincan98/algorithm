ac4b891c1173836e88141f22feb1d025fc262d82
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-08-25 10:33:37 +0900

    [Silver II] Title: 최소 힙, Time: 1416 ms, Memory: 33676 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		for (int i = 0; i < N; i++) {
+			int a = Integer.parseInt(br.readLine());
+			
+			if (a == 0) {
+				if (pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(pq.poll());
+				}
+			} else {
+				pq.add(a);
+			}
+		}
+	}
+}
