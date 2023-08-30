7beeef9dda3f19f0ce510b8836306b504419d066
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-08-30 15:36:24 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 80 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int N = Integer.parseInt(br.readLine());
+		
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		int answer = 0;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if (map.containsKey(a) && map.get(a) != b) {
+				answer++;
+			}
+			map.put(a, b);
+		}
+		System.out.println(answer);
+	}
+}
