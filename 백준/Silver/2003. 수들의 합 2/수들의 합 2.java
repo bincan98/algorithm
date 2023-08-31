778f1e7e40651b01fb5be567c0decf43a6f25856
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-08-31 22:47:47 +0900

    [Silver IV] Title: 수들의 합 2, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,47 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
+/**
+1 2 3 4 2 5 3 1 1 2
+s
+       e
+1 < 5
+3 < 5
+6 > 5
+5 == 5
+	cnt++
+	start++
+9 > 5
+7 > 5
+inchiworm
+ */
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	private static int N, M;
-	private static int[] arr;
-	private static int answer;
 	
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N];
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-		} // 입력완료
+		}
 		
+		// 입력 완료
 		
-		int start = 0; // 시작 지점
-		int end = 0; // 끝 지점
-		int sum = arr[0]; // 부분 수열의 합
-		answer = 0; // 경우의 수 
-		while(true) {
-			if(sum > M) { // 부분수열의 합이 목표보다 크다면
-				sum -= arr[start]; // 시작지점의 값을 빼주고
-				start++; // 시작지점 땡기기
-			} else { // sum <= M // 크거나 같다면
-				if(sum == M) { // 같다면 
-					answer++; // 경우의수 증가시키고
-				}
-				end++; // 끝지점 밀기
-				if(end >= N) break; // 끝에 오면 나가기
-				sum += arr[end]; // 부분수열의 합에서 끝지점값 더해주기
+		int cnt = 0;
+		int start = 0;
+		int end = 0;
+		int sum = arr[start];
+		
+		while (true) {
+			if (sum == m) { // 1.
+				cnt++;
+				end++; // end == n
+				if (end == n) break;
+				sum += arr[end]; 
+			} else if (sum > m) { // 2.
+				sum -= arr[start];
+				start++;
+			} else { // 3.
+				end++;
+				if (end == n) break;
+				sum += arr[end]; 
 			}
 		}
 		
-		System.out.println(answer);
+		System.out.println(cnt);
 		
-	} // end of main
-} // end of class
+	}
+}
