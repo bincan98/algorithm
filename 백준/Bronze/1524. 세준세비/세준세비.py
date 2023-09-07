32d21762f471acea0f3e9a9062ab62693ac67431
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-09-07 22:55:57 +0900

    [Bronze I] Title: 세준세비, Time: 332 ms, Memory: 140464 KB -BaekjoonHub

---
+++ 백준/Bronze/1524. 세준세비/세준세비.py
@@ -0,0 +1,34 @@
+"""
+"""
+
+import sys
+from collections import deque
+
+input = lambda: sys.stdin.readline().strip()
+
+T = int(input())
+
+for i in range(T):
+    input()
+    N, M = map(int, input().split())
+    sejun = deque(sorted(list(map(int, input().split()))))
+    sebi = deque(sorted(list(map(int, input().split()))))
+
+    # sejun = sorted(sejun)
+    # sebi = sorted(sebi)
+
+    sejun_pointer = 0
+    sebi_pointer = 0
+
+    while sebi and sejun:
+        if sejun[0] < sebi[0]:
+            sejun.popleft()
+        else:
+            sebi.popleft()
+
+    if sejun:
+        print("S")
+    elif sebi:
+        print("B")
+    else:
+        print("C")
