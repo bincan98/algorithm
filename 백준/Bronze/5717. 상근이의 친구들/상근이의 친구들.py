bce96edca019e10a80e7e8ecfcd938457477fdb2
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-10-03 00:52:43 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.py
@@ -0,0 +1,12 @@
+"""
+"""
+
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+M, F = map(int, input().split())
+
+while M != 0 and F != 0:
+    print(M + F)
+    M, F = map(int, input().split())
