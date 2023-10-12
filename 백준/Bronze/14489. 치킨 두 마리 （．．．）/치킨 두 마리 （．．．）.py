063a8ea25dc28dfdf90beab7440247fa2f9c1e23
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-10-12 17:51:38 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.py
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.py
@@ -1,17 +1,3 @@
-"""
-냅다 bfs 까지는 아니고..
-지금 칸보다 작으면 앞으로 갈꺼니까 사이클이 발생할 가능성은 없다
-그리고 다음칸에 경우의수 + 1
-
-근데 왜 시간초과 ?
-"""
-import sys
-from collections import deque
-
-dxy = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-
-input = lambda: sys.stdin.readline().strip()
-
 A, B = map(int, input().split())
 C = int(input())
 
