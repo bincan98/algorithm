a7d4f27535d61c9157fd553d21634fbc3f417763
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-11-15 12:44:43 +0900

    [Gold IV] Title: 중복 제거, Time: 1272 ms, Memory: 441768 KB -BaekjoonHub

--- 백준/Gold/13701. 중복 제거/중복 제거.java
+++ 백준/Gold/13701. 중복 제거/중복 제거.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -13,16 +14,16 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		boolean[] check = new boolean[33_554_433];
+		BitSet check = new BitSet();
 		int[] arr = new int[5_000_001];
 		
 		int length = 0;
 		st = new StringTokenizer(br.readLine());
 		while (st.hasMoreTokens()) {
 			int a = Integer.parseInt(st.nextToken());
-			if (!check[a]) {
+			if (!check.get(a)) {
 				sb.append(a + " ");
-				check[a] = true;
+				check.set(a);
 			}
 		}
 		
