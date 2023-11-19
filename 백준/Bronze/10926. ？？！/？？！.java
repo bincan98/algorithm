b95df47850464ab9ea6645f600a589d5d041201c
bincan98 <69081384+bincan98@users.noreply.github.com>
2023-11-19 15:33:12 +0900

    [Bronze V] Title: ??!, Time: 80 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws IOException {
+		
+		String s = br.readLine();
+		
+		System.out.println(s + "??!");
+		
+	}
+}
