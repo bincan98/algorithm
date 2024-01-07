dd020e81c16bb66113c8f32a89e283a652b58441
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-01-07 19:14:16 +0900

    [Bronze I] Title: 소인수분해, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,12 @@
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+N = int(input())
+
+for i in range(2, int(N ** 0.5) + 1):
+    while N % i == 0:
+        print(i)
+        N //= i
+if N != 1:
+    print(N)
