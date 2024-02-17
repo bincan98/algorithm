66302772bed5c6f2370b06205d411901d52dff0c
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-02-17 16:47:26 +0900

    [Bronze I] Title: 소인수분해, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -4,9 +4,11 @@ input = lambda: sys.stdin.readline().strip()
 
 N = int(input())
 
-for i in range(2, int(N ** 0.5) + 1):
-    while N % i == 0:
-        print(i)
-        N //= i
+for p in range(2, int(N ** 0.5) + 1):
+    while N % p == 0:
+        print(p)
+        N = N // p
+    if N == 1:
+        break
 if N != 1:
     print(N)
