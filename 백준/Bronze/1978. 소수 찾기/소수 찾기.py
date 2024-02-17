68efb3866b6faad080da59b64143319ab8ffaba7
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-02-17 16:13:57 +0900

    [Bronze II] Title: 소수 찾기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -10,7 +10,7 @@ for a in lst:
     is_prime = True
     if a == 1:
         continue
-    for i in range(2, a):
+    for i in range(2, int(a ** 0.5) + 1):
         if a % i == 0:
             is_prime = False
             break
