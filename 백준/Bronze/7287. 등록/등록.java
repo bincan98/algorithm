d9f71e09993f688147539f902d97b725b94c4e66
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-02-23 18:50:03 +0900

    [Bronze V] Title: 등록, Time: 68 ms, Memory: 11284 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(538);
+        System.out.println("bincan98");
+
+    } // end of main
+
+} // end of class
