608f9c90feafa49310ea71a617c35ec19bed7218
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-02-25 17:36:38 +0900

    [Bronze IV] Title: Triathlon, Time: 96 ms, Memory: 12172 KB -BaekjoonHub

---
+++ 백준/Bronze/25600. Triathlon/Triathlon.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int result = a * (d + g);
+            if (a == (d + g)) {
+                result *= 2;
+            }
+            answer = Math.max(answer, result);
+        }
+
+        System.out.println(answer);
+
+    } // end of main
+
+} // end of class
