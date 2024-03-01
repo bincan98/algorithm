7b1e68946bf77c17ea63c1cf8ea9e7471b1ed7b5
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-03-01 16:49:06 +0900

    [Bronze V] Title: 오늘 날짜, Time: 112 ms, Memory: 13224 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(LocalDate.now());
+
+    } // end of main
+
+} // end of class
