f8744fdc4ad120cc8b00aaeb10e05988872d7f1d
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-03-03 23:25:34 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 136 ms, Memory: 110924 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -0,0 +1,18 @@
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+N = int(input())
+
+for i in range(N):
+    for j in range(N - i - 1):
+        print(" ", end="")
+    for j in range(i + 1):
+        print("*", end ="")
+    print()
+for i in range(1, N):
+    for j in range(i):
+        print(" ", end="")
+    for j in range(N - i):
+        print("*", end="")
+    print()
