60c870537e92cdb9e6c4717da8e8d72a12d6dcca
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-03-05 18:47:06 +0900

    [Bronze IV] Title: 특별한 날, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,18 @@
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+M = int(input())
+D = int(input())
+
+if M < 2:
+    print("Before")
+elif M > 2:
+    print("After")
+else:
+    if D < 18:
+        print("Before")
+    elif D > 18:
+        print("After")
+    else:
+        print("Special")
