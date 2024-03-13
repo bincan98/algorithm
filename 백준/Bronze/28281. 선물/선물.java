c0e7825f043c1b7a094713f01bbcaa0027381263
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-03-13 23:00:19 +0900

    [Bronze IV] Title: 선물, Time: 212 ms, Memory: 22524 KB -BaekjoonHub

---
+++ 백준/Bronze/28281. 선물/선물.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int mi = arr[0] + arr[1];
+        for (int i = 0; i < N - 1; i++) {
+            mi = Math.min(mi, arr[i] + arr[i + 1]);
+        }
+
+        System.out.println(mi * X);
+
+    } // end of main
+
+} // end of class
