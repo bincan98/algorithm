2e4b2884da8ea6ca956be34087fcba752b9a8599
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-03-17 23:08:04 +0900

    [Bronze III] Title: 스타후르츠, Time: 80 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/17496. 스타후르츠/스타후르츠.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int cur = T + 1;
+        int cnt = 0;
+        while (cur <= N) {
+            cur += T;
+            cnt += 1;
+        }
+
+        System.out.println(cnt * C * P);
+
+    } // end of main
+
+} // end of class
