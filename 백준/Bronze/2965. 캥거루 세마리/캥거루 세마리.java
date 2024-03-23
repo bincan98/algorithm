1494ab7e2203dd79093f982aa6db998bb7468fec
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-03-23 20:57:05 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.max(b - a - 1, c - b - 1));
+
+    } // end of main
+
+} // end of class
