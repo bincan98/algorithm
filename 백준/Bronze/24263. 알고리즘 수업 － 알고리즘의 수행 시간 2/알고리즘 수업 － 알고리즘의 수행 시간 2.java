4cd4346917e7dc70687fde11152bac9453fbd7c2
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-04-04 21:39:47 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N);
+        System.out.println(1);
+
+    } // end of main
+
+} // end of class
