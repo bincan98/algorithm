ce55bb8b6c9bdbdc2bd2106e8b18ad7e620a6ff1
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-04-26 21:45:43 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,17 @@
+"""
+"""
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+s = input()
+if s == "M":
+    print("MatKor")
+elif s == "W":
+    print("WiCys")
+elif s == "C":
+    print("CyKor")
+elif s == "A":
+    print("AlKor")
+else:
+    print("$clear")
