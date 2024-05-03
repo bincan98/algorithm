37283731b74e219a52ea0dc6a15a0d3455a73aa4
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-05-03 21:57:24 +0900

    [Bronze IV] Title: Hello Judge, Time: 92 ms, Memory: 12104 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    private static int A, B, C;
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < N + 1; i++) {
+            System.out.println("Hello World, Judge " + i + "!");
+        }
+
+    } // end of main
+
+} // end of class
