a4ccd170c79ebaecb157a84e9d4052e821a842e6
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-05-10 23:10:29 +0900

    [Bronze V] Title: Rook, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.py
@@ -0,0 +1,19 @@
+"""
+"""
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+print("  ___  ___  ___ ")
+print("  | |__| |__| | ")
+print("  |           | ")
+print("   \_________/ ")
+print("    \_______/ ")
+print("     |     | ")
+print("     |     | ")
+print("     |     | ")
+print("     |     | ")
+print("     |_____| ")
+print("  __/       \__ ")
+print(" /             \ ")
+print("/_______________\ ")
