d65bcfbd171efbade80cf3a536677f0183b7501d
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-05-12 20:11:56 +0900

    [Bronze IV] Title: 모음의 개수, Time: 84 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        String s = br.readLine();
+
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+               answer += 1;
+            }
+        }
+
+        System.out.println(answer);
+
+    } // end of main
+
+} // end of class
