b950d522fe060175d6b1b45b0473da5957630d6c
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-05-26 21:33:28 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.py
@@ -0,0 +1,11 @@
+"""
+"""
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+print("       _.-;;-._")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-;;-._|")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-''-._|")
