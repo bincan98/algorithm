eac8dbab7e67eda1c678eaea72ff6acb92353e42
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-05-28 20:53:24 +0900

    [Bronze V] Title: 콜센터, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.py
@@ -0,0 +1,16 @@
+"""
+"""
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+print(f'     /~\\')
+print(f'    ( oo|')
+print(f'    _\=/_')
+print(f'   /  _  \\')
+print(f'  //|/.\|\\\\')
+print(f' ||  \ /  ||')
+print(f'============')
+print(f'|          |')
+print(f'|          |')
+print(f'|          |')
