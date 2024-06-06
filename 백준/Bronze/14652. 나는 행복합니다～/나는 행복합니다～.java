5875518d3e5b14adfafc72da53e1ca2dc1bfb630
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-06-06 14:24:27 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 76 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(k / m + " " + k % m);
+
+    } // end of main
+
+} // end of class
