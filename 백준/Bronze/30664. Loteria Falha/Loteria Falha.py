c772c23de1d6742c11c0f9769257acae1bb09ff9
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-06-28 20:57:28 +0900

    [Bronze V] Title: Loteria Falha, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30664. Loteria Falha/Loteria Falha.py
@@ -0,0 +1,14 @@
+"""
+"""
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+while True:
+    a = int(input())
+    if a == 0:
+        break
+    if a % 42 == 0:
+        print("PREMIADO")
+    else:
+        print("TENTE NOVAMENTE")
