7fc8495b5291aff87493d9ca7406809e91776e56
bincan98 <69081384+bincan98@users.noreply.github.com>
2024-07-01 21:09:44 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,14 @@
+"""
+"""
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+while True:
+    name, age, weight = input().split()
+    if name == '#':
+        break
+    if int(age) > 17 or int(weight) >= 80:
+        print(name, "Senior")
+    else:
+        print(name, "Junior")
